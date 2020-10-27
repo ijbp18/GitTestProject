8e85e2a75b3e71dcea0d714b73c3cba5992f7e77
Joao Betancourth <ibetancourth@tribalworldwide.gt>
2020-10-27 10:42:21 -0500

    Initial commit

---
+++ app/src/main/java/com/home/gittestproject/model/data.kt
@@ -0,0 +1,7 @@
+package com.home.gittestproject.model
+
+/**
+ * Created by Joao Betancourth on 27,octubre,2020
+ */
+class data {
+}
---
+++ app/src/main/java/com/home/gittestproject/presentation/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.home.gittestproject.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.home.gittestproject.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //Esta info ira para la rama principal (Master) con el fin de validar nuevos cambios en diferentes ramas
+        //version 1
+    }
+}
